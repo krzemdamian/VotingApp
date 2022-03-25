aa3fbd05152ec50a6398c7c9007c2b5d6724baa9
Damian Krzeminski <krzemdamian@gmail.com>
2022-03-25 09:10:37 +0100

    Use rxjs insted subscribing in subscription

--- web/src/app/services/data-provider-service.service.ts
+++ web/src/app/services/data-provider-service.service.ts
@@ -2,43 +2,74 @@ import { Candidate } from './../models/candidate.model';
 import { VotersServiceService } from './voters-service.service';
 import { CandidatesServiceService } from './candidates-service.service';
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, first, Observable } from 'rxjs';
+import { BehaviorSubject, combineLatest, first, Observable, switchMap } from 'rxjs';
 import { Voter } from '../models/voter.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class DataProviderServiceService {
   private _voters: BehaviorSubject<Voter[]> = new BehaviorSubject<Voter[]>([]);
   public voters$: Observable<Voter[]> = this._voters.asObservable();
 
-  private _candidates: BehaviorSubject<Candidate[]> = new BehaviorSubject<Candidate[]>([]);
+  private _candidates: BehaviorSubject<Candidate[]> = new BehaviorSubject<
+    Candidate[]
+  >([]);
   public candidates$: Observable<Candidate[]> = this._candidates.asObservable();
 
   constructor(
     readonly voterService: VotersServiceService,
     readonly candidateService: CandidatesServiceService
-    ) { 
-      this.voterService.getAll().pipe(first()).subscribe(voters => this._voters.next(voters));
-      this.candidateService.getAll().pipe(first()).subscribe(candidates => this._candidates.next(candidates));
-    }
+  ) {
+    this.voterService
+      .getAll()
+      .pipe(first())
+      .subscribe((voters) => this._voters.next(voters));
+    this.candidateService
+      .getAll()
+      .pipe(first())
+      .subscribe((candidates) => this._candidates.next(candidates));
+  }
 
   public addVoter(voter: Voter) {
-    this.voterService.addVoter(voter).pipe(first()).subscribe(() => {
-      this.voterService.getAll().pipe(first()).subscribe(voters => this._voters.next(voters));
-    });
+    this.voterService
+      .addVoter(voter)
+      .pipe(
+        switchMap(() => this.voterService.getAll()),
+        first()
+      )
+      .subscribe((voters) => {
+        this._voters.next(voters);
+      });
   }
 
   public addCandidate(candidate: Candidate) {
-    this.candidateService.addCandidate(candidate).pipe(first()).subscribe(() => {
-      this.candidateService.getAll().pipe(first()).subscribe(candidates => this._candidates.next(candidates));
-    });
+    this.candidateService
+      .addCandidate(candidate)
+      .pipe(
+        first(),
+        switchMap(() => this.candidateService.getAll())
+      )
+      .subscribe((candidates) => {
+        this._candidates.next(candidates);
+      });
   }
 
   public vote(voter: Voter, candidate: Candidate) {
-    this.voterService.vote(voter, candidate).pipe(first()).subscribe(() => {
-      this.voterService.getAll().pipe(first()).subscribe(voters => this._voters.next(voters));
-      this.candidateService.getAll().pipe(first()).subscribe(candidates => this._candidates.next(candidates));
-    });
+    this.voterService
+      .vote(voter, candidate)
+      .pipe(
+        switchMap(() =>
+          combineLatest([
+            this.voterService.getAll(),
+            this.candidateService.getAll(),
+          ])
+        ),
+        first()
+      )
+      .subscribe(([v, c]) => {
+        this._voters.next(v);
+        this._candidates.next(c);
+      });
   }
 }
